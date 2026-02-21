0a3a3806be1ab4d4c7319e796cecb3680cff4335
Amrita <amritap0200@gmail.com>
2026-02-21 23:48:08 +0530

    Backend + async pipeline stable

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -1,7 +1,18 @@
+# backend/celery_app.py
+
 from celery import Celery
 
-celery_app = Celery(
-    "audio_tasks",
+celery = Celery(
+    "deepverify",
     broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    backend="redis://localhost:6379/1"
+)
+
+celery.conf.update(
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    timezone="UTC",
+    enable_utc=True,
+    task_track_started=True
 )
